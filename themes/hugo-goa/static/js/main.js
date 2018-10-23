f973789f269664093cd47ecd76a422d7a74264a2
Tom Spencer <tom.spencer@studiozeffa.com>
2018-10-23 12:19:13 +0100

    Switched to in-built syntax highlighting, added copy button

--- themes/hugo-goa/static/js/main.js
+++ themes/hugo-goa/static/js/main.js
@@ -1,11 +1,41 @@
-// Copyright 2014-2015 Twitter, Inc.
-// Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
-if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+(function() {
+  if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
     var msViewportStyle = document.createElement('style')
     msViewportStyle.appendChild(
-        document.createTextNode(
-            '@-ms-viewport{width:auto!important}'
-        )
+      document.createTextNode(
+              '@-ms-viewport{width:auto!important}'
+      )
     )
     document.querySelector('head').appendChild(msViewportStyle)
-}
+  }
+
+  function flashCopyMessage(btn, msg) {
+    btn.textContent = msg;
+    setTimeout(function() {
+      btn.textContent = "Copy";
+    }, 1000);
+  }
+
+  function addCopyButton(containerEl) {
+    var code = containerEl.textContent;
+
+    var copyBtn = document.createElement("button");
+    copyBtn.className = "highlight-copy-btn";
+    copyBtn.textContent = "Copy";
+    containerEl.append(copyBtn);
+
+    new ClipboardJS(copyBtn, {
+      text: function() {
+        return code;
+      }
+    }).on('success', function() {
+      flashCopyMessage(copyBtn, 'Copied!')
+    }).on('error', function() {
+      flashCopyMessage(copyBtn, 'Not Supported :(')
+    });
+  }
+
+  // Add copy button to code blocks
+  var highlightBlocks = document.getElementsByClassName('highlight');
+  Array.prototype.forEach.call(highlightBlocks, addCopyButton);
+})();
